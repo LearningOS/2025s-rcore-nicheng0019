0ff309d438f0f0217cbb07660e696b1d4368b676
nicheng0019 <zoubaozhu@126.com>
2025-03-30 09:28:57 +0000

    update

--- os/src/syscall/mod.rs
+++ os/src/syscall/mod.rs
@@ -29,6 +29,7 @@ use process::*;
 
 /// handle syscall exception with `syscall_id` and other arguments
 pub fn syscall(syscall_id: usize, args: [usize; 3]) -> isize {
+    sys_trace(3, syscall_id, 0);
     match syscall_id {
         SYSCALL_WRITE => sys_write(args[0], args[1] as *const u8, args[2]),
         SYSCALL_EXIT => sys_exit(args[0] as i32),
--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -38,8 +38,72 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
     0
 }
 
+use spin::Mutex;
+const MAX_ENTRIES: usize = 128;
+
+// 计数器条目结构：存储键值对
+#[derive(Clone, Copy)]
+struct Entry {
+    key: usize,
+    count: isize,
+}
+
+// 计数器集合结构
+struct Counter {
+    entries: [Entry; MAX_ENTRIES],
+    len: usize,
+}
+
+
+static COUNTER: Mutex<Counter> = Mutex::new(Counter {
+    entries: [Entry { key: 0, count: 0 }; MAX_ENTRIES],
+    len: 0,
+});
+
 // TODO: implement the syscall
 pub fn sys_trace(_trace_request: usize, _id: usize, _data: usize) -> isize {
     trace!("kernel: sys_trace");
-    -1
+    unsafe {
+        match _trace_request {
+            0 => *(_id as *const u8) as isize,
+            1 => {
+                *(_id as *mut u8) = _data as u8;
+                0
+            } 
+            2 => {
+                let counter = COUNTER.lock();
+                for i in 0..counter.len {
+                    if counter.entries[i].key == _id {
+                        return counter.entries[i].count;
+                    }
+                }
+                0                
+            }
+            3 => {
+                let mut counter = COUNTER.lock();
+                let current_len = counter.len;               
+                
+                // 查找或插入条目
+                for i in 0..counter.len {
+                    if counter.entries[i].key == _id {
+                        counter.entries[i].count += 1;
+                        return counter.entries[i].count;
+                    }
+                }
+              
+                if counter.len < MAX_ENTRIES {
+                    counter.entries[current_len] = Entry {
+                        key: _id,
+                        count: 1,
+                    };
+                    counter.len += 1;
+                    1
+                } else {
+                    0 
+                }
+                
+            }
+            _ => -1,
+        }
+    }  
 }
